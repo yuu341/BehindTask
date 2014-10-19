2753a56829f9bd9bba3f5e3915a8174996e0890d
Yusuke Furuya <oz.g.341@gmail.com>
2014-10-20 08:58:38 +0900

    簡易タスク管理アプリの箱だけ作成

---
+++ BehindTask/BehindTask/Entity/T_TASK.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BehindTask.Entity
+{
+    class T_TASK
+    {
+        public DateTime Limit { get; set; }
+    }
+}
---
+++ BehindTask/BehindTask/Utils/File/LoadFile.cs
@@ -0,0 +1,24 @@
+﻿using BehindTask.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BehindTask.Utils.File
+{
+    class LoadFile
+    {
+        public List<T_TASK> LoadTasks()
+        {
+            return null;
+        }
+
+        public void SaveTasks(List<T_TASK> target)
+        {
+
+        }
+
+
+    }
+}
